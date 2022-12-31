3d4f3373be9ea8310701899411e7cfb0c152c447
Kushagra Ghildiyal <kushagra183@gmail.com>
2022-12-31 14:38:23 +0530

    structure to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import img from "./components/images/bts.png"
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContainer">
+   
+    <div style={{display:"flex",justifyContent:"center"}}><img src={img} alt="logo" height={"100px"} width="250px" /></div>
+   
+    <div className="AppContainer">      
+      <div className="containerOuter">        
+       {/**movie */}
+       {/**slots */}
+       {/**seats */}
+   
+ {/**submit btn */}
+
+       </div>
+    <div >
+      {/**lastBooking and clear button here */}
+     </div> 
+    </div>
     </div>
   );
 }
